5c5477a7656b1fddecc5ba0fe68a2d6ba34dea3c
ismaelbob <bobismaeljg@gmail.com>
2021-04-29 16:32:55 -0400

    Se creo un nuevo navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,49 @@
 import React from 'react'
+import './styles/navbar.css'
 import {Link} from 'react-router-dom'
+import logo from '../img/UEC.png'
 
 class Navbar extends React.Component {
     state = {
-        menuActivo: 0
+        menuActivo: '0',
+        toggle: false,
     }
 
+    handleClickToggle = (event) => {
+        const menuSandwich = document.querySelector('#links')
+        if(event.target.tagName === 'A') {
+            this.setState({menuActivo: event.target.name, toggle: false})
+            menuSandwich.classList.remove('active')
+        } else {
+            if (this.state.toggle) {
+                menuSandwich.classList.remove('active')
+                this.setState({toggle: false})
+            } else {
+                menuSandwich.classList.add('active')
+                this.setState({toggle: true})
+            }
+        }
+    }
 
     render () {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primario">
-                <div className="navbar-brand">Unidos en Cristo</div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className={`nav-item ${this.props.activo === 'Inicio' ? 'active' : ''}`} onClick={this.props.onChangeMenu}>
-                            <Link className="nav-link" to="/">Inicio</Link>
-                        </li>
-                        <li className={`nav-item ${this.props.activo === 'Cancionero' ? 'active' : ''}`}>
-                            <Link className="nav-link" onClick={this.props.onChangeMenu} to="/cancionero">Cancionero</Link>
-                        </li>
-                        <li className={`nav-item ${this.props.activo === 'Noticias' ? 'active' : ''}`}>
-                            <Link className="nav-link" onClick={this.props.onChangeMenu} to="/noticias">Noticias</Link>
-                        </li>
-                        <li className={`nav-item ${this.props.activo === 'Ofrendas' ? 'active' : ''}`}>
-                            <Link className="nav-link" onClick={this.props.onChangeMenu} to="/ofrendas">Ofrendas</Link>
-                        </li>
-                    </ul>
+            <div className="box_navbar">
+                <div className="box_navbar-logo"><img src={logo} alt="Logo"/></div>
+                <div className="box_navbar-link" id="links">
+                    <div className="box_navbar-link-left">
+                        <h4>Unidos en Cristo</h4>
+                        <Link name="0" to="/" onClick={this.handleClickToggle} className={this.state.menuActivo === '0' ? 'activo' : ''}>Inicio</Link>
+                        <Link name="1" to="/cancionero" onClick={this.handleClickToggle} className={this.state.menuActivo === '1' ? 'activo' : ''}>Cancionero</Link>
+                        <Link name="2" to="/noticias" onClick={this.handleClickToggle} className={this.state.menuActivo === '2' ? 'activo' : ''}>Noticias</Link>
+                        <Link name="3" to="/cronograma" onClick={this.handleClickToggle} className={this.state.menuActivo === '3' ? 'activo' : ''}>Cronograma</Link>
+                    </div>
+                    <div className="box_navbar-link-right">
+                        <Link name="4" to="/login" onClick={this.handleClickToggle} className={this.state.menuActivo === '4' ? 'activo' : ''}>Iniciar sesion</Link>
+                        <button onClick={this.handleClickToggle}>&times;</button>
+                    </div>
                 </div>
-            </nav>
+                <div className="box_navbar-toggle"><button onClick={this.handleClickToggle}>&#9776;</button></div>
+            </div>
         )
     }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -11,13 +11,7 @@ class Home extends React.Component {
             <div>
                 <div className="container mt-2">
                     <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-ride="carousel">
-                        <ol className="carousel-indicators">
-                            <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
-                        </ol>
+                        
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <img src={img3} className="d-block w-100" alt="..."/>
@@ -64,6 +58,74 @@ class Home extends React.Component {
                             <span className="sr-only">Next</span>
                         </a>
                     </div>
+                </div>
+
+                <div className="container mt-2">
+                    <div className="bg-light shadow">
+                        <div className="text-primary fs-2 text-center">Bienvenido!</div>
+                    </div>
+                </div>
+
+                <div className="container my-2">
+                    <div className="bg-light p-3">
+                        <div className="text-primary fs-3 text-center mb-3">NUESTRAS REUNIONES</div>
+                        <div className="row">
+                            <div className="col-12 col-md-3 mb-md-0 mb-2">
+                                <div className="card text-white bg-info h-100">
+                                    <div className="card-header text-center">DOMINGOS</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">10:00</h5>
+                                        <p className="card-text">Escuela dominical.</p>
+                                        <h5 className="card-title">19:00</h5>
+                                        <p className="card-text">Culto Central.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-3 mb-md-0 mb-2">
+                                <div className="card text-white bg-info h-100">
+                                    <div className="card-header text-center">MARTES</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">19:00</h5>
+                                        <p className="card-text">Noche de oración.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-3 mb-md-0 mb-2">
+                                <div className="card text-white bg-info h-100">
+                                    <div className="card-header text-center">JUEVES</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">14:00</h5>
+                                        <p className="card-text">Culto de Union Femenil "Lea".</p>
+                                        <h5 className="card-title">20:00</h5>
+                                        <p className="card-text">Noche de estudio bíblico.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-3 mb-md-0 mb-2">
+                                <div className="card text-white bg-info h-100">
+                                    <div className="card-header text-center">SABADO</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">19:00</h5>
+                                        <p className="card-text">Culto de Union de Jóvenes "Heroes de la Fe"</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="container">
+                    <div className="bg-light">
+                        <div>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d944.720444256612!2d-66.10751173010414!3d-17.447048262684724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sbo!4v1611193718733!5m2!1ses!2sbo" style={{'border': 'none'}} width="100%" height="400" allowFullScreen="" aria-hidden="false" tabIndex="0" title="ubicacion"></iframe>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container bg-dark text-light text-center p-3 mt-2">
+                    <p>Iglesia Evangelica Bautista - "Unidos en Cristo</p>
+                    <p>Cochabamba - Bolivia<br></br>2021</p>
                 </div>
             </div>
         )

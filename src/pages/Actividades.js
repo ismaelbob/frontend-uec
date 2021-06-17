58f4662e42184364dd47c829437faba8db99e668
ismaelbob <bobismaeljg@gmail.com>
2021-06-17 12:08:55 -0400

    Aumente cronograma de junio y julio

--- src/pages/Actividades.js
+++ src/pages/Actividades.js
@@ -32,13 +32,13 @@ class Actividades extends React.Component {
                                 <div class="row no-gutters">
                                     <div class="card-body">
                                         <h6 class="card-title text-primary">Practicas de canto</h6>
-                                        <p class="card-text">Fechas: Miércoles 5, 12, 19 y 26 de mayo <br/>Hora: 19:00 <br/>Lugar: Ambientes de la Iglesia</p>
+                                        <p class="card-text">Fechas: Miércoles 2, 9, 16, 23 y 30 de junio <br/>Hora: 19:00 <br/>Lugar: Ambientes de la Iglesia</p>
                                         <p class="card-text"><small class="text-muted">Para cantantes de los ministerios de alabanza.</small></p>
 
                                         <div className="separador"></div>
 
                                         <h6 class="card-title text-primary">Taller de teoria musical</h6>
-                                        <p class="card-text">Fecha: Lúnes 24 de mayo <br/>Hora: 19:00 <br/>Lugar: Ambientes de la Iglesia</p>
+                                        <p class="card-text">Fecha: Lúnes 28 de junio <br/>Hora: 19:00 <br/>Lugar: Ambientes de la Iglesia</p>
                                         <p class="card-text"><small class="text-muted">Para integrantes de los ministerios de alabanza.</small></p>
                                     </div>
                                 </div>
@@ -49,26 +49,17 @@ class Actividades extends React.Component {
 
                 <div className="container mt-3  ">
                     <div className="head_titulo">ROL DE DIRECCION</div>
+
                     <div className="card mt-2">
-                        <h5 className="card-header">Mayo</h5>
+                        <h5 className="card-header">Junio</h5>
                         <div className="card-body">
-                            <div className="row border-bottom p-2">
-                                <div className="col-5 col-sm-6">Ministerio Even-Ezer</div>
-                                <div className="col-7 col-sm-6">
-                                    <div className="row">
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Domingo 2</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g2 my-sm-0 my-1">Martes 4</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Jueves 6</div>
-                                    </div>
-                                </div>
-                            </div>
                             <div className="row border-bottom p-2">
                                 <div className="col-5 col-sm-6">Ministerio Oriel</div>
                                 <div className="col-7 col-sm-6">
                                     <div className="row">
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g3">Domingo 9</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g3 my-sm-0 my-1">Martes 11</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g3">Jueves 13</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g3">Domingo 6</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g3 my-sm-0 my-1">Martes 8</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g3">Jueves 10</div>
                                     </div>
                                 </div>
                             </div>
@@ -76,9 +67,9 @@ class Actividades extends React.Component {
                                 <div className="col-5 col-sm-6">Ministerio Hno. Ismael</div>
                                 <div className="col-7 col-sm-6">
                                     <div className="row">
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g4">Domingo 16</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g4 my-sm-0 my-1">Martes 18</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g4">Jueves 20</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g4">Domingo 13</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g4 my-sm-0 my-1">Martes 15</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g4">Jueves 17</div>
                                     </div>
                                 </div>
                             </div>
@@ -86,9 +77,9 @@ class Actividades extends React.Component {
                                 <div className="col-5 col-sm-6">Ministerio Cuerdas</div>
                                 <div className="col-7 col-sm-6">
                                     <div className="row">
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g1">Domingo 23</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g1 my-sm-0 my-1">Martes 25</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g1">Jueves 27</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g1">Domingo 20</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g1 my-sm-0 my-1">Martes 22</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g1">Jueves 24</div>
                                     </div>
                                 </div>
                             </div>
@@ -96,9 +87,9 @@ class Actividades extends React.Component {
                                 <div className="col-5 col-sm-6">Ministerio Even-Ezer</div>
                                 <div className="col-7 col-sm-6">
                                     <div className="row">
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Domingo 30</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g2 my-sm-0 my-1">Martes 1 junio</div>
-                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Jueves 3 junio</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Domingo 27</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g2 my-sm-0 my-1">Martes 29</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Jueves 1 de Jul</div>
                                     </div>
                                 </div>
                             </div>
@@ -106,16 +97,74 @@ class Actividades extends React.Component {
                                 <div className="col-4 col-sm-2">Culto de jóvenes</div>
                                 <div className="col-8 col-sm-10">
                                     <div className="row">
-                                        <div className="col-12 col-sm-3 rounded-pill bg-g2">Sab. 1 Even-Ezer</div>
-                                        <div className="col-12 col-sm-3 rounded-pill bg-g4 my-sm-0 mt-1">Sab. 8 Ismael</div>
-                                        <div className="col-12 col-sm-3 rounded-pill bg-g3 my-sm-0 my-1">Sab. 15 Oriel</div>
-                                        <div className="col-12 col-sm-3 rounded-pill bg-g2 my-sm-0">Sab. 22 Even-Ezer</div>
-                                        <div className="col-12 col-sm-3 rounded-pill bg-g4 my-sm-0">Sab. 29 Ismael</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g2">Sab. 5 Even-Ezer</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g4 my-sm-0 mt-1">Sab. 12 Ismael</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g3 my-sm-0 my-1">Sab. 19 Oriel</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g2 my-sm-0">Sab. 26 Even-Ezer</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <div className="card mt-2">
+                        <h5 className="card-header">Julio</h5>
+                        <div className="card-body">
+                            <div className="row border-bottom p-2">
+                                <div className="col-5 col-sm-6">Ministerio Oriel</div>
+                                <div className="col-7 col-sm-6">
+                                    <div className="row">
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g3">Domingo 4</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g3 my-sm-0 my-1">Martes 6</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g3">Jueves 8</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row border-bottom p-2">
+                                <div className="col-5 col-sm-6">Ministerio Hno. Ismael</div>
+                                <div className="col-7 col-sm-6">
+                                    <div className="row">
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g4">Domingo 11</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g4 my-sm-0 my-1">Martes 13</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g4">Jueves 15</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row border-bottom p-2">
+                                <div className="col-5 col-sm-6">Ministerio Cuerdas</div>
+                                <div className="col-7 col-sm-6">
+                                    <div className="row">
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g1">Domingo 18</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g1 my-sm-0 my-1">Martes 20</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g1">Jueves 22</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row border-bottom p-2">
+                                <div className="col-5 col-sm-6">Ministerio Even-Ezer</div>
+                                <div className="col-7 col-sm-6">
+                                    <div className="row">
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Domingo 25</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g2 my-sm-0 my-1">Martes 27</div>
+                                        <div className="col-12 col-sm-4 rounded-pill bg-g2">Jueves 29</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row border-bottom p-2">
+                                <div className="col-4 col-sm-2">Culto de jóvenes</div>
+                                <div className="col-8 col-sm-10">
+                                    <div className="row">
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g4">Sab. 3 Hno. Ismael</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g3 my-sm-0 mt-1">Sab. 10 Oriel</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g2 my-sm-0 my-1">Sab. 17 Even-Ezer</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g4 my-sm-0">Sab. 24 Hno. Ismael</div>
+                                        <div className="col-12 col-sm-3 rounded-pill bg-g3 my-sm-0">Sab. 31 Oriel</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         )

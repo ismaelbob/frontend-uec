27529bb7552c89fbf39feaa4678cb3c3e6c5e3a2
bob <bobismaeljg@gmail.com>
2022-07-19 23:15:03 -0400

    pequeña correccion para el menu activo y al seleccionar himnario

--- src/context/menuactivo/Provider.js
+++ src/context/menuactivo/Provider.js
@@ -1,23 +1,13 @@
 import MenuActivoContext from './index'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function MenuActivoProvider ({children}) {
     const pagina = localStorage.getItem('pagina')
 
     const [page, setPage] = useState(pagina)
 
-    useEffect(()=>{
-        setPage(pagina)
-        // eslint-disable-next-line
-    },[])
-
-    useEffect(()=>{
-        setPage(pagina)
-    },[pagina])
-
     const handleClick = (event) => {
         setPage(event.target.id)
-        console.log(event.target.id)
     }
     
     return (

4858944705dcfd78b8ad07cbd4b2e99cf76b341e
ismaelbob <74365597+ismaelbob@users.noreply.github.com>
2022-12-20 18:28:06 -0400

    Correccion en config.js me equivoque la url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    'urlapi': 'https://https://uecapi.000webhostapp.com/'
+    'urlapi': 'https://uecapi.000webhostapp.com/'
     //'urlapi': 'https://uecapi.herokuapp.com'
     //'urlapi': 'http://app-07b4677d-3b8b-4041-81f4-c7ed616266d4.cleverapps.io'
 }

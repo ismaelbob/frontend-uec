0257387b070027cd7a50f5ff3f76ed48124ec615
ismaelbob <74365597+ismaelbob@users.noreply.github.com>
2024-12-05 22:15:25 -0400

    Corrigiendo ultimo commit
    Olvide colocar nombre de variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    'http://uecapi.free.nf/'
+    'urlapi': 'http://uecapi.free.nf/'
     //'urlapi': 'https://uecapi.000webhostapp.com/'
     //'urlapi': 'https://uecapi.herokuapp.com'
     //'urlapi': 'http://app-07b4677d-3b8b-4041-81f4-c7ed616266d4.cleverapps.io'

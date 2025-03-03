342e4733dc3cc4ca2165eaf4675e4e22861b3ab7
Bob <bobismaeljg@gmail.com>
2025-03-03 15:31:07 -0400

    Correccion en ServiceWorker para que consuma de cache si no hay respuesta de red

--- src/service-worker.js
+++ src/service-worker.js
@@ -26,50 +26,51 @@ precacheAndRoute(self.__WB_MANIFEST);
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
-  // Devuelva falso para eximir las solicitudes de ser cumplidas por index.html.
+  // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
-    // Si esto no es una navegación, omita.
+    // If this isn't a navigation, skip.
     if (request.mode !== 'navigate') {
       return false;
-    } // Si esta es una URL que comienza con / _, omítala.
+    } // If this is a URL that starts with /_, skip.
 
     if (url.pathname.startsWith('/_')) {
       return false;
-    } // Si parece una URL para un recurso, porque contiene // una extensión de archivo, omita.
+    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
     if (url.pathname.match(fileExtensionRegexp)) {
       return false;
-    } // Devuelve verdadero para indicar que queremos usar el controlador.
+    } // Return true to signal that we want to use the handler.
 
     return true;
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
-// Un ejemplo de ruta de almacenamiento en caché en tiempo de ejecución para solicitudes que no son manejadas por el 
-// precaché, en este caso solicitudes .png del mismo origen como las de public /
+// An example runtime caching route for requests that aren't handled by the
+// precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-  // Agregue cualquier otra extensión de archivo o criterio de enrutamiento según sea necesario.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Personalice esta estrategia según sea necesario, por ejemplo, cambiando a CacheFirst.
+  // Add in any other file extensions or routing criteria as needed.
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
-      // Asegúrese de que una vez que esta caché en tiempo de ejecución alcance un tamaño máximo, 
-      // se eliminen las imágenes utilizadas menos recientemente.
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
     ],
   })
 );
 
-// Esto permite que la aplicación web active skipWaiting a través de registration
-// .waiting.postMessage ({tipo: 'SKIP_WAITING'})
+// This allows the web app to trigger skipWaiting via
+// registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
 
-// Cualquier otra lógica de service worker personalizado puede ir aquí.
+// Any other custom service worker logic can go here.
+
 const datosCanciones = [
   'https://uecapi.elementfx.com/himjovenes/getcanciones.php',
   'https://uecapi.elementfx.com/himpoder/getcanciones.php',
@@ -88,27 +89,35 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).then(networkResponse => {
-      // Verificar si la respuesta de la red es válida
-      if (!networkResponse || networkResponse.status !== 200 || (networkResponse.type !== 'basic' && networkResponse.type !== 'cors')) {
-        return caches.match(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      // Si hay una respuesta en caché, devolverla
+      if (cachedResponse) {
+        return cachedResponse;
       }
 
-      // Clonar la respuesta de la red
-      const networkResponseClone = networkResponse.clone();
+      // Si no hay respuesta en caché, intentar obtener los datos de la red
+      return fetch(event.request).then(networkResponse => {
+        // Verificar si la respuesta de la red es válida
+        if (!networkResponse || networkResponse.status !== 200 || (networkResponse.type !== 'basic' && networkResponse.type !== 'cors')) {
+          return caches.match(event.request);
+        }
 
-      // Comparar los datos de la red con los datos en caché
-      return caches.open('memoria-v1').then(cache => {
-        return cache.match(event.request).then(cachedResponse => {
-          if (!cachedResponse || !responsesAreEqual(cachedResponse, networkResponseClone)) {
-            cache.put(event.request, networkResponseClone);
-          }
-          return networkResponse;
+        // Clonar la respuesta de la red
+        const networkResponseClone = networkResponse.clone();
+
+        // Comparar los datos de la red con los datos en caché
+        return caches.open('memoria-v1').then(cache => {
+          return cache.match(event.request).then(cachedResponse => {
+            if (!cachedResponse || !responsesAreEqual(cachedResponse, networkResponseClone)) {
+              cache.put(event.request, networkResponseClone);
+            }
+            return networkResponse;
+          });
         });
+      }).catch(() => {
+        // Si la red falla, devolver la respuesta en caché
+        return caches.match(event.request);
       });
-    }).catch(() => {
-      // Si la red falla, devolver la respuesta en caché
-      return caches.match(event.request);
     })
   );
 });
